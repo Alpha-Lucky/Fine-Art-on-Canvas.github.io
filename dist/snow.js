4603e5d8b5427b6bd8183ad3246f3fabfe90066e
okmic <okmic.g@gmail.com>
2025-01-27 21:59:15 +0300

    SNOWFLAKE

--- dist/snow.js
+++
@@ -1,9 +0,0 @@
-for (let i = 0; i < 100; i++) {
-    const snowflake = document.createElement('div');
-    snowflake.className = 'snowflake';
-    snowflake.style.left = Math.random() * 100 + 'vw'; // Случайная позиция по горизонтали
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'; // Случайная скорость падения
-    snowflake.style.opacity = Math.random(); // Случайная прозрачность
-    snowflake.style.animationDelay = Math.random() * 5 + 's'; // Случайная задержка
-    document.body.appendChild(snowflake);
-}
