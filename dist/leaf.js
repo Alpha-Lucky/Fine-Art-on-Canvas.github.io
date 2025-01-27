08fada2a1b89ce8d37172131d19e1cabcc9637d1
okmic <okmic.g@gmail.com>
2025-01-27 19:42:10 +0300

    leaf limit

--- dist/leaf.js
+++ dist/leaf.js
@@ -1,27 +1,24 @@
 let leavesInterval
 let leavesActive = true
+const maxLeaves = 6.9
+let currentLeavesCount = 0
 
 function createLeaf() {
-    const leaf = document.createElement("img")
-    leaf.src = "dist/leaf.png"
-    leaf.className = "leaf"
-    leaf.style.left = Math.random() * 100 + "vw"
-    leaf.style.top = "-50px"
-    leaf.style.animationDuration = Math.random() * 3 + 2 + "s"
-    document.body.appendChild(leaf)
+    if (currentLeavesCount < maxLeaves) {
+        const leaf = document.createElement("img")
+        leaf.src = "dist/leaf.png"
+        leaf.className = "leaf"
+        leaf.style.left = Math.random() * 100 + "vw"
+        leaf.style.top = "-50px"
+        leaf.style.animationDuration = Math.random() * 3 + 2 + "s"
+        document.body.appendChild(leaf)
+        currentLeavesCount++
 
-    leaf.addEventListener("animationend", () => {
-        leaf.remove()
-    })
+        leaf.addEventListener("animationend", () => {
+            leaf.remove()
+            currentLeavesCount--
+        })
+    }
 }
 
-function startLeaves() {
-    leavesInterval = setInterval(createLeaf, 3690)
-}
-
-function stopLeaves() {
-    clearInterval(leavesInterval)
-    leavesActive = false
-}
-
-startLeaves()
+leavesInterval = setInterval(createLeaf, 3690)
