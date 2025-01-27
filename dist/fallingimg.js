737041b3d37a9986337697aa30db85511f7d8ce5
okmic <okmic.g@gmail.com>
2025-01-27 22:31:31 +0300

    rm SNOWFLAKE

--- dist/fallingimg.js
+++ dist/fallingimg.js
@@ -2,27 +2,15 @@ let leavesInterval
 let leavesActive = true
 let maxLeaves
 let currentLeavesCount = 0
-let snowflakeCont = 50
 
 function setMaxLeaves() {
     if (window.innerWidth < 850) {
         maxLeaves = 3 
-        snowflakeCont = 30
     } else {
         maxLeaves = 6.9
-        snowflakeCont = 100
     }
 }
 
-for (let i = 0; i < snowflakeCont; i++) {
-    const snowflake = document.createElement('div')
-    snowflake.className = 'snowflake'
-    snowflake.style.left = Math.random() * 100 + 'vw'
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'
-    snowflake.style.opacity = Math.random()
-    snowflake.style.animationDelay = Math.random() * 5 + 's'
-    document.body.appendChild(snowflake)
-}
 
 
 function createFallingImg() {
